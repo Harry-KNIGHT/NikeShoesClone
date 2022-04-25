401faf1f0cea198e4941106db30366016a7292b1
Harry-KNIGHT <eliott.knight@icloud.com>
2022-04-25 11:25:12 +0200

    Adding main page, challenge done in hour

--- NikeClone/ContentView.swift
+++ NikeClone/ContentView.swift
@@ -8,45 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    let columns = [
-            GridItem(.adaptive(minimum: 150))
-        ]
-
     var body: some View {
-        ScrollView(showsIndicators: false) {
-            LazyVGrid(columns: columns, spacing: 40) {
-                ForEach(shoes) { shoe in
-                    VStack(alignment: .leading) {
-                        ZStack(alignment: .topTrailing) {
-                    Image(shoe.image)
-                        .resizable()
-                        .scaledToFill()
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                            Image(systemName: "heart")
-                                .padding(5)
-                                .foregroundColor(Color.black)
-                                .font(.system(size: 20))
-                                .background(Color.white)
-                                .clipShape(Circle())
-                                .padding(10)
-                        }
-                        Text(shoe.name)
-                            .font(.headline)
-                            .padding(.vertical, 1)
-                    Text(shoe.description)
-                            .foregroundColor(.secondary)
-                            .padding(.bottom, 1)
-                        HStack {
-                        Text(String(shoe.price))
-                                .padding(0)
-                                .font(.headline)
-                        Text(shoe.currency)
-                                .font(.headline)
-                        }
-                    }
+        TabView {
+            MainPageView()
+                .tabItem {
+                    Label("Accueil", systemImage: "house")
                 }
-            }
-            .padding(.horizontal)
+            
+            ShoesView()
+                .tabItem {
+                    Label("Voir", systemImage: "magnifyingglass")
+                }
         }
     }
 }
---
+++ NikeClone/ShoesView.swift
@@ -0,0 +1,59 @@
+//
+//  ShoesView.swift
+//  NikeClone
+//
+//  Created by Elliot Knight on 25/04/2022.
+//
+
+import SwiftUI
+
+struct ShoesView: View {
+    let columns = [
+            GridItem(.adaptive(minimum: 150))
+        ]
+
+    var body: some View {
+        ScrollView(showsIndicators: false) {
+            LazyVGrid(columns: columns, spacing: 40) {
+                ForEach(shoes) { shoe in
+                    VStack(alignment: .leading) {
+                        ZStack(alignment: .topTrailing) {
+                    Image(shoe.image)
+                        .resizable()
+                        .scaledToFill()
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                            Image(systemName: "heart")
+                                .padding(5)
+                                .foregroundColor(Color.black)
+                                .font(.system(size: 20))
+                                .background(Color.white)
+                                .clipShape(Circle())
+                                .padding(10)
+                        }
+                        Text(shoe.name)
+                            .font(.headline)
+                            .padding(.vertical, 1)
+                    Text(shoe.description)
+                            .foregroundColor(.secondary)
+                            .padding(.bottom, 1)
+                        HStack {
+                        Text(String(shoe.price))
+                                .padding(0)
+                                .font(.headline)
+                        Text(shoe.currency)
+                                .font(.headline)
+                        }
+                    }
+                }
+            }
+            .padding(.horizontal)
+        }
+    }
+}
+
+struct ShoesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShoesView()
+            .preferredColorScheme(.dark)
+    }
+}
